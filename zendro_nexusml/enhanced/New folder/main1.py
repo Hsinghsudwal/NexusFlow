a19d4cf8a2243032f68db878462383e724e6d2e4
Hsinghsudwal <sudwalh@gmail.com>
2025-03-25 17:49:22 -0700

    update

--- zendro_nexusml/enhanced/New folder/main1.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    # Path to your dataset (replace with your actual file path)
-    data_path = "path_to_your_data.csv"
-    
-    # Create an instance of the Pipeline and run it
-    pipeline = Pipeline(data_path)
-    pipeline.run()
--- zeningml/src/data_loader.py
+++
@@ -1,9 +0,0 @@
-from core.artifact_store import ArtifactStore
-
-class DataLoader:
-    def __init__(self, config):
-        self.artifact_store = ArtifactStore(config)
-        
-    def load_data(self):
-        data = self.artifact_store.load_artifact("raw_data", "dataset.csv")
-        return data
